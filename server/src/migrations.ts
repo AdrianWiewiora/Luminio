6936c592a9da68e4c78d80343902f808eb479094
Kacper <48856951+n0tcnj1eck4h@users.noreply.github.com>
2025-03-11 09:04:20 +0100

    Move migrations to their own directory

--- server/src/migrations.ts
+++ server/src/migrations.ts
@@ -1,49 +1,42 @@
-import { PendingQuery } from "postgres";
 import { sql } from "./db.ts";
+import { readDir } from "@std/fs/unstable-read-dir";
+import * as path from "@std/path";
 
-const MIGRATIONS = new Map<string, PendingQuery<object[]>>();
-
-MIGRATIONS.set(
-  "2025-03-10-0000 initial user schema",
-  sql`CREATE TABLE "users" (
-    "id" SERIAL PRIMARY KEY,
-    "first_name" varchar,
-    "last_name" varchar,
-    "email" varchar UNIQUE,
-    "password_hash" varchar,
-    "user_description" text,
-    "created_at" timestamp DEFAULT (now())
-  );`,
-);
-
-MIGRATIONS.set(
-  "2025-03-10-0010 initial photos schema",
-  sql`CREATE TABLE "photos" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" integer NOT NULL,
-    "title" varchar,
-    "description" text,
-    "created_at" timestamp DEFAULT (now())
-  );`,
-);
+const MIGRATIONS_DIR = "./../migrations/";
 
 interface LuminioDbInfo {
   value: string;
 }
 
 export async function migrate() {
+  // Bootstrap migration info in the db
   await sql`CREATE TABLE IF NOT EXISTS luminio_db_info (key TEXT PRIMARY KEY, value TEXT)`;
   await sql`INSERT INTO luminio_db_info (key, value) VALUES ('last_migration', '') ON CONFLICT (key) DO NOTHING`;
 
+  // Fetch the last migration
   const rows = await sql<
     LuminioDbInfo[]
   >`SELECT value FROM luminio_db_info WHERE key = 'last_migration'`;
   let last_migration = rows[0].value;
 
-  for (const [migration, query] of MIGRATIONS) {
+  // Iterate over all migrations and run them if they're newer than the last db migration
+  const dir = readDir(MIGRATIONS_DIR);
+  const file_names: string[] = [];
+  for await (const file of dir) {
+    if (file.isFile && file.name.endsWith(".sql")) {
+      file_names.push(file.name);
+    }
+  }
+
+  file_names.sort((a, b) => a.localeCompare(b));
+
+  for (const file_name of file_names) {
+    const migration = file_name.split(".sql")[0];
     if (migration <= last_migration) {
       continue;
     }
+
+    const query = sql.file(path.join(MIGRATIONS_DIR, file_name));
 
     console.log(`Running migration ${migration}`);
     await sql.begin(async (sql) => {
