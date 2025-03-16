43c291403efdae7048266992f9ab8c1ee552569d
AdiAs <adi7878787@gmail.com>
2025-03-16 18:39:45 +0100

    Added new config.docker.ts and repair dockerfile to use it.

--- server/config.example.ts
+++ server/config.example.ts
@@ -1,24 +1,4 @@
-let config;
-
-if (Deno.env.get("IS_DOCKER")) {
-    config = {
-        MIGRATIONS_DIR: "../../migrations/",
-        DATABASE_URL: "postgres://postgres:1234@postgres/luminio",
-        HOSTNAME: "0.0.0.0",
-        PORT: 8000,
-    };
-} 
-else {
-    config = {
-        MIGRATIONS_DIR: "../../migrations/",
-        DATABASE_URL: "postgres://username:password@127.0.0.1/luminio",
-        HOSTNAME: "127.0.0.1",
-        PORT: 8000,
-    };
-}
-
-export const MIGRATIONS_DIR = config.MIGRATIONS_DIR;
-export const DATABASE_URL = config.DATABASE_URL;
-export const HOSTNAME = config.HOSTNAME;
-export const PORT = config.PORT;
-
+export const MIGRATIONS_DIR = "../../migrations/";
+export const DATABASE_URL = "postgres://username:password@127.0.0.1/luminio";
+export const HOSTNAME = "127.0.0.1";
+export const PORT = 8000;
