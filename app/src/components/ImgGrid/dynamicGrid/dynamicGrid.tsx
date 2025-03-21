7c3b9a226626b19eb5a2d5c7e96662c3fc2de16f
Szymon Szlęk <103324293+szymonis019@users.noreply.github.com>
2025-03-21 17:38:53 +0100

    hotfix scss
    converting css to scss, removing unused component,

--- app/src/components/ImgGrid/dynamicGrid/dynamicGrid.tsx
+++ app/src/components/ImgGrid/dynamicGrid/dynamicGrid.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./dynamicGrid.scss";
 import { image1, image2, image3, image4, image5, image6, image7, image8 } from "../../../assets/img/imgExport.tsx";
 
@@ -8,13 +7,13 @@ const allImages = [...Array(3)].flatMap(() => [
 
 const DynamicGrid = () => {
   return (
-    <div className="grid-container-dynamic">
+    <section className="grid-container-dynamic">
       {allImages.map((img, index) => (
-        <div key={index} className="grid-item-dynamic">
+        <div key={index} className="grid-container-dynamic__grid-item-dynamic">
           <img src={img} alt={`Dynamic Grid item ${index}`} loading="lazy" />
         </div>
       ))}
-    </div>
+    </section>
   );
 };
 
