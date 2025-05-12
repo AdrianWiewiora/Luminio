c3bcee1d406a209943e6570ee9b069e7d2b407c3
AdiAs <adi7878787@gmail.com>
2025-05-12 23:28:15 +0200

    Change configuration and connection to /api/me

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,14 +6,15 @@ import process from "node:process";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  logLevel: 'error',
   server: {
     host: process.env.IS_DOCKER === 'true' ? '0.0.0.0' : '127.0.0.1',
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.IS_DOCKER === 'true' ? 'http://server:8000' : 'http://127.0.0.1:8000',
+        target: process.env.IS_DOCKER === 'true' ? 'http://server-container:8000' : 'http://127.0.0.1:8000',
         changeOrigin: true,
-        rewrite: (path) => path,
+        rewrite: (path) => path
       }
     },
     watch: {
